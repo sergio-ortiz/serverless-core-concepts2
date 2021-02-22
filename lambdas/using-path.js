40e9c043db50dbd522a6b31d2ee978ce99f4cf73
Sergio Ortiz <ortiz.sergio0303@gmail.com>
2021-02-21 20:48:26 -0500

    feat(lambdas): interpolate the last part of the event path into the returned html

--- lambdas/using-path.js
+++ lambdas/using-path.js
@@ -1,7 +1,9 @@
 exports.handler = async function (event, context) {
+  const path = event.path.split("/").pop();
+
   return {
     statusCode: 200,
     headers: { "content-type": "text/html" },
-    body: "<h1>Hello World</h1>",
+    body: `<h1>Hello ${path}</h1>`,
   };
 };
